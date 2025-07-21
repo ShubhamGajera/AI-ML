c76a7526682b98a68cca16e1b03c604470ecadde
Shubham_Gajera <shubhamgajera122@gmail.com>
2025-07-21 23:12:20 +0530

    04 Intials

--- Codedex.py
+++ Codedex.py
@@ -1,1 +1,15 @@
 print('hello')
+
+
+#04 Intials 
+# Write code below 💖
+#My name is shubham gajera
+# i like to read managa..
+print('SSSSS   GGGGGG')
+print('S       G    G')
+print('S       G     ')
+print('SSSSS   G  GGG')
+print('    S   G    G')
+print('    S   G    G')
+print('SSSSS   GGGGGG')
+
