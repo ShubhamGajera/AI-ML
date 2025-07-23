74af3b390183f2d815bdb773554277594da6d91d
Shubham_Gajera <shubhamgajera122@gmail.com>
2025-07-23 21:14:19 +0530

    11 Coin Flip

--- Codedex.py
+++ Codedex.py
@@ -165,3 +165,16 @@ print(d)
 # total = pesos * 0.00025 + soles * 0.28 + reais * 0.21
 
 # print(total)
+
+#11 Coin flip
+
+# Write code below 💖
+
+import random
+
+num = random.randint(0, 1)   # Generates a random number that's either 0 or 1
+
+if num > 0.5:
+  print('Heads')
+else:
+  print('Tails')
