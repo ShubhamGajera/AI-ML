52d1e64e800ea4a5bd95961f27105de22d0974ee
Shubham_Gajera <shubhamgajera122@gmail.com>
2025-07-23 22:06:25 +0530

    13 pH Levels

--- Codedex.py
+++ Codedex.py
@@ -196,3 +196,26 @@ if(grade >= 55):
   print("You Passed✌️")
 else:
   print("You failed")
+
+
+# 13 pH Levels
+
+# Relational Operators
+# A lot of the times inside conditions, we are comparing two values. To do so, we 
+# need to use a different type of operators called relational operators that compares values:
+# == equal to
+# != not equal to
+# > greater than
+# < less than
+# >= greater than or equal to
+# <= less than or equal to
+
+
+ph = int(input("Write a value of pH"))
+
+if(ph>7):
+  print("Basic")
+elif(ph<7):
+  print("Acidic")
+else:
+  print("Neutral")
