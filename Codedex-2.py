0eab1174861308a3a88da30e814afba15d09ccba
Shubham_Gajera <shubhamgajera122@gmail.com>
2025-07-24 23:15:24 +0530

    18 Guess Number

--- Codedex-2.py
+++ Codedex-2.py
@@ -9,3 +9,19 @@ while pin != 1234:
 
 if pin == 1234:
   print('PIN accepted!')
+
+
+# 18. Guess Number
+# Write code below 💖
+
+guess = 0
+tries = 0
+
+while guess != 6 and tries<5:
+  guess = int(input("Guess the number:  "))
+  tries = tries + 1
+
+if guess != 6:
+  print('You ran out of tries.')
+else:
+  print('You got it!')
