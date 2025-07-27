abd36089b92b85777f6ea603d15a14bbb4134d6d
Shubham_Gajera <shubhamgajera122@gmail.com>
2025-07-27 23:12:51 +0530

    Complteing first part...

---
+++ Projects/rock_paper_scissors.py
@@ -0,0 +1,16 @@
+import random
+computer = random.randint(1,3)
+print("===================")
+print("Rock Paper Scissors")
+print("===================")
+print("1) ✊")
+print("2) ✋")
+print("3) ✌️")
+player = int(input("Pick Number..."))
+
+if (computer==1 and player==2):
+    print('You Choose ✊')
+    print('CPU Choose ✋')
+    print('You Win..')
+
+print("You Choose " + )
