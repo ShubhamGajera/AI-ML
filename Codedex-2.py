e325a0c920dc00eb83a88d948aab58a91114d1c6
Shubham_Gajera <shubhamgajera122@gmail.com>
2025-07-29 23:01:47 +0530

    Recap

--- Codedex-2.py
+++ Codedex-2.py
@@ -183,3 +183,13 @@ things_to_do = [
 
 for i in things_to_do:
   print(i)
+
+
+# Recap of old Chepter
+
+# Lists are used to store different items in a single variable.
+# An index is an item's position in a list. Python lists are 0-indexed.
+# Slicing can access certain parts of a list with name[start:end].
+# Python has built-in functions like len(), max(), min().
+# Lists have built-in methods like .append(), .insert(), .remove(), .pop().
+# We can iterate over a list using for-in.
