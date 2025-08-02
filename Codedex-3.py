911a1e9df0871c28b071650d8001839d12aece45
Shubham_Gajera <shubhamgajera122@gmail.com>
2025-08-02 21:57:18 +0530

    29 Fortune Cookie

--- Codedex-3.py
+++ Codedex-3.py
@@ -1,2 +1,25 @@
 # 29. Fortune Cookie
 
+# Write code below 💖
+import random
+
+def fortune():
+  a=random.randint(0,7)
+  if(a==0):
+    print("Don't pursue happiness – create it.")
+  elif(a==1):
+    print("All things are difficult before they are easy.")
+  elif(a==2):
+    print("The early bird gets the worm, but the second mouse gets the cheese.")
+  elif(a==3):
+    print("Someone in your life needs a letter from you.")
+  elif(a==4):
+    print("Don't just think. Act!")
+  elif(a==5):
+    print("Your heart will skip a beat.")
+  elif(a==6):
+    print("The fortune you search for is in another cookie.")
+  else:
+    print("Help! I'm being held prisoner in a Chinese bakery!")
+
+fortune()
