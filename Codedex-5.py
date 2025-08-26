230a9fd9fb26460710a006ee9746fd16d0d4036a
Shubham_Gajera <shubhamgajera122@gmail.com>
2025-08-26 23:25:59 +0530

    39 Slot Machine - codedex github

--- Codedex-5.py
+++ Codedex-5.py
@@ -18,3 +18,38 @@ def play():
     plays=input('If want to play Write "Y" if not than "N" :')
 
 play()
+
+
+# codedex
+
+# Slot Machine 🎰
+# Codédex
+
+import random
+
+symbols = [
+  '🍒',
+  '🍇',
+  '🍉',
+  '7️⃣'
+]
+
+def play():
+
+  for i in range(1, 51):    
+    results = random.choices(symbols, k=3)
+    print(f'{results[0]} | {results[1]} | {results[2]}')
+    win = results[0] == '7️⃣' and results[1] == '7️⃣' and results[2] == '7️⃣'
+
+    if win:
+      print('Jackpot!!! 💰')
+      break
+    else:
+      results = random.choices(symbols, k=3)
+
+answer = ''
+while answer.upper() != 'N':
+  play()
+  answer = input('Keep playing? (Y/N) ')
+
+print('Thanks for playing!')
