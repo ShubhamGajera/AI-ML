bf9dda6de03aa48da599c089ee6c100648cf4343
Shubham_Gajera <shubhamgajera122@gmail.com>
2025-08-27 23:09:48 +0530

    41 Countdown

---
+++ 41 - pyhon/main.py
@@ -0,0 +1,11 @@
+import datetime
+from bday_messages import random_message
+
+
+today=datetime.date.today()
+next_birthday=datetime.date(2026,7,25)
+days_away = next_birthday - today
+if today==next_birthday:
+  print(random_message)
+else:
+  print(f'My next birthday is {days_away.days} days away!')
